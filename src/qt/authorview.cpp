716dfc7fababcf28a474f85f3c6060bee68342a1
CryptAxe <cryptaxe@gmail.com>
2015-12-21 23:32:06 -0800

    Added code to launch combo, decision, and market creation widget

--- src/qt/authorview.cpp
+++ src/qt/authorview.cpp
@@ -1,6 +1,9 @@
 #include "authorview.h"
 #include "ui_authorview.h"
 
+#include <QHBoxLayout>
+#include <QDialog>
+
 AuthorView::AuthorView(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::AuthorView)
@@ -15,15 +18,34 @@ AuthorView::~AuthorView()
 
 void AuthorView::on_pushButtonCreateCombo_clicked()
 {
+    ComboCreationWidget *comboCreationWidget = new ComboCreationWidget(this);
 
+    QHBoxLayout *hbox = new QHBoxLayout(this);
+    hbox->addWidget(comboCreationWidget);
+
+    QDialog *dialog = new QDialog(this);
+    dialog->setLayout(hbox);
+    dialog->show();
 }
 
 void AuthorView::on_pushButtonCreateDecision_clicked()
 {
+    DecisionCreationWidget *creationWidget = new DecisionCreationWidget(this);
+    QHBoxLayout *hbox = new QHBoxLayout(this);
+    hbox->addWidget(creationWidget);
 
+    QDialog *dialog = new QDialog(this);
+    dialog->setLayout(hbox);
+    dialog->show();
 }
 
 void AuthorView::on_pushButtonCreateMarket_clicked()
 {
+    DecisionMarketCreationWidget *marketCreationWidget = new DecisionMarketCreationWidget(this);
+    QHBoxLayout *hbox = new QHBoxLayout(this);
+    hbox->addWidget(marketCreationWidget);
 
+    QDialog *dialog = new QDialog(this);
+    dialog->setLayout(hbox);
+    dialog->show();
 }
